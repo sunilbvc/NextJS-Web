7192ca0819d9103bf62b3887d27ffdacb1046139
Sunil Besra <sunil.besra@vamaship.com>
2025-08-18 19:04:07 +0530

    file updated

--- app/page.tsx
+++ app/page.tsx
@@ -23,7 +23,7 @@ export default function Home() {
       <Pricing />
       <WhyChooseUs />
       <TechnologyStack />
-      <BrandsSlider />
+      {/* <BrandsSlider /> */}
       <About />
       <Portfolio />
       <Testimonials />
